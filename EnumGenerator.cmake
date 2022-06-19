97c94b3ff55a8a9fb10830b166a232ae172aa907
Nicolas IRAGNE <nicolas.iragne@alyce.fr>
2022-06-19 14:06:34 +0200

    Use Find_Package for python interpreter

--- EnumGenerator.cmake
+++ EnumGenerator.cmake
@@ -12,11 +12,15 @@
 
 cmake_minimum_required(VERSION 3.17)
 
+if (NOT Python3_EXECUTABLE)
+    Find_Package(Python3 COMPONENTS Interpreter REQUIRED)
+endif()
+
 function(Generate_Enum)
 
-set(GENERATOR ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/EnumGenerator.py)
-set(GENERATOR_HEADERS ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/EnumPattern.h.in)
-set(GENERATOR_SOURCES ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/EnumPattern.cpp.in)
+set(GENERATOR ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/EnumGenerator.py)
+set(GENERATOR_HEADERS ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/EnumPattern.h.in)
+set(GENERATOR_SOURCES ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/EnumPattern.cpp.in)
 set(GENERATED_SOURCES_FOLDER "${CMAKE_CURRENT_BINARY_DIR}/generated-src")
 
 message(STATUS "Generating enum files...")
@@ -28,9 +32,9 @@ foreach(ENUM_JSON ${ARGN})
     add_custom_command(
         OUTPUT ${HEADER_OUT} ${CPP_OUT}
         DEPENDS ${ENUM_JSON} ${GENERATOR} ${GENERATOR_HEADERS} ${GENERATOR_SOURCES}
-        COMMAND python ${GENERATOR} ${ENUM_JSON} ${GENERATED_SOURCES_FOLDER}
+        COMMAND ${Python3_EXECUTABLE} ${GENERATOR} ${ENUM_JSON} ${GENERATED_SOURCES_FOLDER}
         COMMENT "Generating ${HEADER_OUT} and ${CPP_OUT} from ${ENUM_JSON}")
-    add_library(${FILENAME} ${GENERATED_SOURCES})
+    add_library(${FILENAME} ${HEADER_OUT} ${CPP_OUT})
     set_target_properties(${FILENAME} PROPERTIES PUBLIC_HEADER ${HEADER_OUT})
     target_include_directories(
         ${FILENAME} PUBLIC
